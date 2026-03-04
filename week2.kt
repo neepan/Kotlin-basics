bff840e120d069331cf851910c8058e76551b4ba
Neepan <neepanbiswas@gmail.com>
2026-03-04 10:35:59 +0530

    week 2 phase 1 completed

---
+++ week2.kt
@@ -0,0 +1,24 @@
+fun String.addHastag(): String{
+return "#${this}"
+}
+//--------------
+//or
+fun String.addHashtag() = "#$this"
+
+//----------------------------------------------------
+
+val myTransaction = listOf(
+Transaction(id= 1, amount=150.0, category="Food"),
+Transaction(id = 2, amount = 800.0, category = "Rent"),
+Transaction(id = 3, amount = 50.0, category = "Coffee")
+)
+
+val foodTransactoins = myTransaction.filter{ it.category =="Food"}
+
+
+//-----------------------------------------------
+
+transaction?.let{
+    println("${it.amount}")
+    println("Transaction Loaded")
+}
